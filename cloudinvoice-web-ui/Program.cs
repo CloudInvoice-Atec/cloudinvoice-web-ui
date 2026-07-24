6a3e5654f64c162ebbf9ea0a90be55bf63f92e5a
Mario Oliveira <Mario.Oliveira.T0133601@edu.atec.pt>
2026-07-24 20:14:02 +0100

    Implementação do handler que injeta jwt no header do pedido http

--- cloudinvoice-web-ui/Program.cs
+++ cloudinvoice-web-ui/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddTransient<JwtAuthorizationHandler>();
+
 // HttpClient para a Identity.API
 builder.Services.AddHttpClient("IdentityAPI", client =>
 {
@@ -16,13 +18,15 @@ builder.Services.AddHttpClient("IdentityAPI", client =>
 builder.Services.AddHttpClient("CatalogAPI", client =>
 {
     client.BaseAddress = new Uri(builder.Configuration["ApiUrls:CatalogApi"] ?? "https://localhost:5003");
-});
+})
+    .AddHttpMessageHandler<JwtAuthorizationHandler>();
 
 // HttpClient para a Billing.API
 builder.Services.AddHttpClient("BillingAPI", client =>
 {
     client.BaseAddress = new Uri(builder.Configuration["ApiUrls:BillingApi"] ?? "https://localhost:5005");
-});
+})
+    .AddHttpMessageHandler<JwtAuthorizationHandler>();
 
 var app = builder.Build();
 
