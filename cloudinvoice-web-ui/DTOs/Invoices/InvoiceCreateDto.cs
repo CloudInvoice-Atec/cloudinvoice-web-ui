361646e502f82588156e0775abebd89d97335d31
JorgeDias97 <78178645+JorgeDias97@users.noreply.github.com>
2026-09-02 22:48:20 +0100

    Inoice Changes

---
+++ cloudinvoice-web-ui/DTOs/Invoices/InvoiceCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using static cloudinvoice_web_ui.Components.Pages.Backoffice.Invoices.Invoice;
+
+namespace cloudinvoice_web_ui.DTOs.Invoices
+{
+    public class InvoiceCreateDto
+{
+        public Guid? ClientId { get; set; }
+        public string Reference { get; set; }
+        public DateTime DateEmission { get; set; } = DateTime.Today;
+        public DateTime DateDue { get; set; } = DateTime.Today.AddDays(30);
+        public string Notes { get; set; }
+        public List<InvoiceLineDto> Lines { get; set; } = new List<InvoiceLineDto>();
+    }
+}
